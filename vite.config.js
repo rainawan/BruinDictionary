6ae0f1e06f9635360aefc2619991a19e1cdd8646
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-01-27 01:01:49 -0800

    change base dir

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()]
+  plugins: [react(), eslint()],
+  base: '/src/'
 });
