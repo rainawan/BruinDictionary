f22090fbe7419d5fec300a6e06e9322e2dfae454
awllms <antcodess@gmail.com>
2024-02-04 13:22:09 -0800

    Adds initial firebase setup. Firebase is loading on main page displaying data from database. Remove vite eslint as it was causing errors. We should put it back on once the database is fully flesh out and we can resolve the errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,65 @@
-import { useState } from 'react';
+import { collection, query, where, getDocs, terminate } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Home from './pages/Home';
+import db from '../src/utils/firebase';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [entries, setEntries] = useState([]);
+  const [terms, setTerms] = useState({});
+  const [users, setUsers] = useState([]);
 
+  useEffect(() => {
+    async function fetchEntries() {
+      const querySnapshot = await getDocs(collection(db.db, 'Entries'));
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data());
+        setEntries([...entries, { id: doc.id, ...doc.data() }]);
+      });
+    }
+    fetchEntries();
+  }, []);
+
+  useEffect(() => {
+    async function fetchTerms() {
+      const querySnapshot = await getDocs(collection(db.db, 'Terms'));
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data());
+        setTerms({...terms, [doc.id]: doc.data().name});
+      });
+    }
+    fetchTerms();
+  }, []);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const querySnapshot = await getDocs(collection(db.db, 'Users'));
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data());
+        setUsers([...users, { id: doc.id, ...doc.data() }]);
+      });
+    }
+  }, []);
+  console.log(entries);
   return (
     <>
-      <Home></Home>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h1>Bruin Dictionary</h1>
+      {entries.map((entry, index) => (
+        <div key={index}>
+          <h2>{terms[entry.termid]}</h2>
+          <p>{entry.example}</p>
+        </div>
+      ))}
+      <h2>Add New Entry</h2>
+      <div className="text-box">
+        <select id='term-select'>
+          {Object.keys(terms).map(key => <option key={key} value={terms[key]}>{terms[key]}</option>)}
+        </select>
+        <input className="example-text-box" />
+        <textarea className="example-text-box" />
+        <button>Add Entry</button>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
