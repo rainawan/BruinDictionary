ad089c8fe71d1f86c3ca4056eda822d2eace611a
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-02-08 17:22:52 -0800

    create user data provider and context hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import { useState } from 'react';
+import useCurrentUserData from './utils/useCurrentUserData';
 import './App.css';
-import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { userData, setUserData } = useCurrentUserData();
 
   return (
-    <>
-      <Home></Home>
-      <div></div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="App">
+      <button
+        onClick={() => {
+          setUserData({ username: 'John Doe' });
+        }}>
+        {userData?.username ?? 'click to update user name'}
+      </button>
+    </div>
   );
 }
 
