744b10dedf5709e4dcfe77e7219bb5dd17da40b5
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-02-08 20:18:51 -0800

    add lodash debounce

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
+import debounce from 'lodash.debounce';
 import useCurrentUserData from '../utils/useCurrentUserData';
 
 const Home = () => {
   const { userData, setUserData } = useCurrentUserData();
+  const [searchTerm, setSearchTerm] = useState('haha');
+
+  const handleSearch = (term) => {
+    setSearchTerm(() => term);
+    debounce(() => {});
+  };
+  const debouncedSearch = debounce(handleSearch, 500);
 
   return (
     <div>
+      HOME
       <button
         onClick={() => {
           setUserData({ username: 'John Doe' });
         }}>
         {userData?.username ?? 'click to update user name'}
       </button>
-      HOME
+      <input type="text" value={searchTerm} onChange={(e) => handleSearch(e.target.value)} />
     </div>
   );
 };
