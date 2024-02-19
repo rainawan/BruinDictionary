7160cbb0df8a7a16e4d16c12b3bf1d96a3a7ee79
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-02-18 17:49:19 -0800

    revert changes

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,47 +1,28 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { Autocomplete, AutocompleteItem, Input } from '@nextui-org/react';
 import { SearchOutlined, LoadingOutlined, WarningOutlined } from '@ant-design/icons';
-import getTermsQuery from '../utils/getTermsQuery';
-import { unpackTermsQuery } from '../utils/unpackQuery';
+
+const items = [
+  {
+    termid: '0',
+    name: 'UCLA',
+    description: 'University of California, Los Angeles'
+  },
+  {
+    termid: '1',
+    name: 'YRL',
+    description: 'Young Research Library'
+  }
+];
 
 const Searchbar = () => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const searchTerm = searchParams.get('term');
 
-  const termsQuery = getTermsQuery();
-  const { status: termsStatus, data: terms } = unpackTermsQuery(termsQuery);
-
-  if (termsStatus === 'LOADING') {
-    return (
-      <Input
-        isDisabled
-        classNames={{ inputWrapper: 'h-[3rem]' }}
-        aria-label="searchbar loading"
-        variant="bordered"
-        radius="full"
-        startContent={<LoadingOutlined />}
-        endContent={<SearchOutlined className="text-xl mt-[-3px]" />}
-      />
-    );
-  } else if (termsStatus === 'ERROR') {
-    return (
-      <Input
-        isInvalid
-        isDisabled
-        classNames={{ inputWrapper: 'h-[3rem]' }}
-        aria-label="searchbar error"
-        variant="bordered"
-        radius="full"
-        startContent={<WarningOutlined style={{ color: 'red' }} />}
-        endContent={<SearchOutlined className="text-xl mt-[-3px]" />}
-      />
-    );
-  }
-
   const handleSelectionChange = (termid) => {
     if (termid) {
-      navigate(`/?term=${terms[termid]}`);
+      navigate(`/?term=${termid}`);
     } else {
       navigate('/');
     }
@@ -53,7 +34,7 @@ const Searchbar = () => {
       variant="bordered"
       radius="full"
       menuTrigger="input"
-      defaultItems={Object.entries(terms)}
+      defaultItems={items}
       defaultInputValue={searchTerm}
       onSelectionChange={handleSelectionChange}
       classNames={{ selectorButton: 'hidden' }}
@@ -75,11 +56,11 @@ const Searchbar = () => {
         }
       }}
       endContent={<SearchOutlined className="text-xl mr-2 mt-[-3px]" />}>
-      {([termid, termname]) => (
-        <AutocompleteItem key={termid} textValue={termname}>
+      {(item) => (
+        <AutocompleteItem key={item.termid} textValue={item.name}>
           <div className="flex gap-2 items-center">
-            <div>{termname}</div>
-            {/* <div className="truncate">{item.definition}</div> */}
+            <div className="font-bold">{item.name}</div>
+            <div className="truncate">{item.description}</div>
           </div>
         </AutocompleteItem>
       )}
--- src/pages/Dictionary.jsx
+++ src/pages/Dictionary.jsx
@@ -1,50 +1,33 @@
 import { useSearchParams } from 'react-router-dom';
-import { unpackEntriesQuery, unpackTermsQuery } from '../utils/unpackQuery';
-import { getTermsEntriesStatus } from '../utils/getTermsEntriesStatus';
-import getEntriesQuery from '../utils/getEntriesQuery';
-import getTermsQuery from '../utils/getTermsQuery';
+import { fetchEntries, fetchTerms, fetchUsers } from '../utils/fetchData';
 import CardLoading from '../components/CardLoading';
 
 const Dictionary = () => {
   const [searchParams] = useSearchParams();
   const searchTerm = searchParams.get('term')?.toLowerCase();
 
-  const termsQuery = getTermsQuery(searchTerm);
-  const { status: termsStatus, data: terms } = unpackTermsQuery(termsQuery);
-  const termid = searchTerm && terms ? Object.keys(terms)[0] : undefined;
+  const entries = fetchEntries();
+  const terms = fetchTerms();
+  const users = fetchUsers();
+  console.log('entries: ', entries, '\nterms: ', terms, '\nusers: ', users);
 
-  const entriesQuery = getEntriesQuery({ termid });
-  const { status: entriesStatus, data: entries } = unpackEntriesQuery(entriesQuery);
-
-  if (searchTerm && !termid) {
-    return <div>not found</div>;
-  }
-
-  const status = getTermsEntriesStatus(termsStatus, entriesStatus);
-
-  switch (status) {
-    case 'SUCCESS':
-      return (
-        <div className="Terms">
-          {entries.map((entry, index) => (
-            <div key={index}>
-              <h2>{terms[entry.termid]}</h2>
-              <h3>Definition</h3>
-              <p>{entry.definition}</p>
-              <h3>Example</h3>
-              <p>{entry.example}</p>
-              <br />
-            </div>
-          ))}
-        </div>
-      );
-    case 'LOADING':
-      return <CardLoading />;
-    case 'ERROR':
-      return <div>error occurred</div>;
-    default:
-      throw new Error('Unhandled status');
-  }
+  return (
+    <div className="Terms">
+      {entries && terms && users ? (
+        entries.map((entry, index) => (
+          <div key={index}>
+            <h2>{terms[entry.termid]}</h2>
+            <h3>Definition</h3>
+            <p>{entry.definition}</p>
+            <h3>Example</h3>
+            <p>{entry.example}</p>
+          </div>
+        ))
+      ) : (
+        <div>loading...</div>
+      )}
+    </div>
+  );
 };
 
 export default Dictionary;
