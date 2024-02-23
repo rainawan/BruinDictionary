c1664c4ac1bf7a4c16257d687ab6738f3b8c6f15
rainawan <rainawan02@gmail.com>
2024-02-22 17:32:34 -0800

    create Text class for Dictionary.jsx, style header desc and example

--- src/pages/Dictionary/Dictionary.jsx
+++ src/pages/Dictionary/Dictionary.jsx
@@ -10,16 +10,33 @@ const Dictionary = () => {
   const users = fetchUsers();
   console.log('entries: ', entries, '\nterms: ', terms, '\nusers: ', users);
 
+  const Text = ({ h1, h2, h3, className, children }) => {
+    const h1classname = h1 ? 'text-4xl font-bold text-yellow-200 text-left mb-3' : '';
+    const h2classname = h2 ? 'text-1.8xl font-bold text-left' : '';
+    const h3classname = h3 ? 'text-1.5xl text-left ml-5' : '';
+    return (
+      <p className={`${h1classname} ${h2classname} ${h3classname} ${className}`}>{children}</p>
+    );
+  };
+
   return (
     <div className="Terms">
       {entries && terms && users ? (
         entries.map((entry, index) => (
-          <div key={index}>
-            <h2>{terms[entry.termid]}</h2>
-            <h3>Definition</h3>
-            <p>{entry.definition}</p>
-            <h3>Example</h3>
-            <p>{entry.example}</p>
+          <div key={index} className="mb-4">
+            <div className="bg-blue-900 p-5 border">
+              <Text h1 className="term">
+                {terms[entry.termid]}
+              </Text>
+              <Text h2>Definition</Text>
+              <Text h3 className="definition">
+                {entry.definition}
+              </Text>
+              <Text h2>Example</Text>
+              <Text h3 className="example">
+                {entry.example}
+              </Text>
+            </div>
           </div>
         ))
       ) : (
