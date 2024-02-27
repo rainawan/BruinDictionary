fa74bc1e5c4a4c08fd288b2a38ea6b2f76f1b426
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-02-26 23:08:17 -0800

    edit path error

--- src/pages/SignIn/components/GoogleSignInButton.jsx
+++ src/pages/SignIn/components/GoogleSignInButton.jsx
@@ -1,5 +1,5 @@
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { auth } from '../utils/firebase';
+import { auth } from '../../../utils/firebase';
 import { Button } from '@nextui-org/react';
 
 const GoogleSignInButton = () => {
