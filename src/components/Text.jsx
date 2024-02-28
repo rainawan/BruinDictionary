3d347c3ece4cb8f93fd8f50a54c1ab9ec257f06f
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-02-27 22:10:59 -0800

    minor change

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,12 +1,11 @@
-const Text = ({ h1, h2, h3, h4, h5, h6, className, children }) => {
-  const sizes = [h1, h2, h3, h4, h5, h6];
+const Text = ({ h1, h2, h3, h4, h5, className, children }) => {
+  const sizes = [h1, h2, h3, h4, h5];
   const styles = [
-    'text-9xl',
-    'text-8xl',
     'text-6xl',
     'text-4xl leading-snug',
     'text-2xl leading-snug',
-    'text-xl'
+    'text-xl',
+    'text-lg'
   ];
   const textClassName =
     sizes.map((size, index) => (size ? styles[index] : null)).find(Boolean) || '';
