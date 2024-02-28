4c8973ec92a8fc6a84f148a5411ac1741e1ce017
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-02-28 11:42:56 -0800

    use useRef

--- src/pages/Dictionary/components/EditMode.jsx
+++ src/pages/Dictionary/components/EditMode.jsx
@@ -1,19 +1,38 @@
+import { useRef } from 'react';
 import { Button, Textarea } from '@nextui-org/react';
 
 const EditMode = ({ entry, setEditEntryid }) => {
+  const newDefinition = useRef(entry.definition);
+  const newExample = useRef(entry.example);
+
   const handleEditCancel = () => {
     setEditEntryid(undefined);
   };
 
   const handleEditConfirm = () => {
     // TODO: handle edit confirm
+    console.log(newDefinition.current, newExample.current);
   };
 
   return (
     <>
-      <Textarea defaultValue={entry.definition} minRows={1} maxRows={4} />
+      <Textarea
+        minRows={1}
+        maxRows={4}
+        defaultValue={entry.definition}
+        onValueChange={(value) => {
+          newDefinition.current = value;
+        }}
+      />
       <p className="mt-3 mb-1 text-md md:text-lg font-medium">Example</p>
-      <Textarea defaultValue={entry.example} minRows={1} maxRows={4} />
+      <Textarea
+        minRows={1}
+        maxRows={4}
+        defaultValue={entry.example}
+        onValueChange={(value) => {
+          newExample.current = value;
+        }}
+      />
       <div className="mt-5 inline-flex flex-row gap-1">
         <Button variant="light" onClick={handleEditConfirm}>
           Confirm
