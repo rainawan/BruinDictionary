deec5ab7573e602c660c757dbcf035e5fbb45480
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-03-04 00:13:28 -0800

    create Add to List button

--- src/pages/Dictionary/components/DisplayMode.jsx
+++ src/pages/Dictionary/components/DisplayMode.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Button } from '@nextui-org/react';
 import { LikeFilled, DislikeFilled } from '@ant-design/icons';
+import FlagButton from './FlagButton';
 
 const DisplayMode = ({ entry }) => {
   const [feedback, setFeedback] = useState({ like: false, dislike: false });
@@ -17,19 +18,22 @@ const DisplayMode = ({ entry }) => {
       <p className="md:text-lg">{entry.definition}</p>
       <p className="mt-3 mb-1 md:text-lg font-medium">Example</p>
       <p className="italic md:text-lg">{entry.example}</p>
-      <div className="mt-5 inline-flex flex-row gap-1">
-        <Button
-          className={`hover:text-green-500 ${feedback.like ? 'bg-green-500 text-white' : ''}`}
-          onClick={() => handleClick('like')}>
-          <LikeFilled className="text-lg" />
-          <p className="text-black dark:text-white">{entry.likes}</p>
-        </Button>
-        <Button
-          className={`hover:text-red-500 ${feedback.dislike ? 'bg-red-500 text-white' : ''}`}
-          onClick={() => handleClick('dislike')}>
-          <DislikeFilled className="text-lg" />
-          <p className="text-black dark:text-white">{entry.dislikes}</p>
-        </Button>
+      <div className="mt-5 inline-flex flex-row flex-wrap place-content-between w-full">
+        <div className="inline-flex flex-row gap-1">
+          <Button
+            className={`hover:text-green-500 ${feedback.like ? 'bg-green-500 text-white' : ''}`}
+            onClick={() => handleClick('like')}>
+            <LikeFilled className="text-lg" />
+            <p className="text-black dark:text-white">{entry.likes}</p>
+          </Button>
+          <Button
+            className={`hover:text-red-500 ${feedback.dislike ? 'bg-red-500 text-white' : ''}`}
+            onClick={() => handleClick('dislike')}>
+            <DislikeFilled className="text-lg" />
+            <p className="text-black dark:text-white">{entry.dislikes}</p>
+          </Button>
+        </div>
+        <FlagButton entryid={entry.id} />
       </div>
     </>
   );
---
+++ src/pages/Dictionary/components/FlagButton.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@nextui-org/react';
+import { CheckOutlined, FlagFilled } from '@ant-design/icons';
+import useCurrentUserData from '../../../utils/useCurrentUserData';
+
+const FlagButton = ({ entryid }) => {
+  const [isFlagged, setIsFlagged] = useState(false);
+  const { userData } = useCurrentUserData();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (!userData) {
+      navigate('/user');
+      return;
+    }
+    // TODO: Add to list
+    setIsFlagged((prev) => !prev);
+  };
+  return (
+    <Button className="min-w-0 transition-all" variant="light" radius="full" onClick={handleClick}>
+      <div className="inline-flex gap-2 place-items-center">
+        {isFlagged ? (
+          <>
+            <p className="hidden sm:block">Added</p>
+            <CheckOutlined className="text-xl" />
+          </>
+        ) : (
+          <>
+            <p className="hidden sm:block">Add to List</p>
+            <FlagFilled className="text-xl" />
+          </>
+        )}
+      </div>
+    </Button>
+  );
+};
+
+export default FlagButton;
