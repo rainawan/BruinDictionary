6eb7c18e964e417fdb0d94cd363f9af34b03bb45
Khanh Nguyen <323.kvn@gmail.com>
2024-03-04 21:46:56 -0800

    minor changes to header info

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,6 +1,8 @@
 import useCurrentUserData from '../../utils/useCurrentUserData';
 import ProfileAvatar from '../../components/ProfileAvatar';
 import SignOutButton from './components/SignOutButton';
+import Text from '../../components/Text';
+import { Image, Avatar } from '@nextui-org/react';
 
 const Profile = () => {
   const { userData } = useCurrentUserData();
@@ -9,11 +11,25 @@ const Profile = () => {
     <section className="Login">
       <div className="mx-auto text-white text-center place-content-center flex items-stretch flex-col">
         <div className="mx-auto w-full p-2 place-content-center flex">
-          <ProfileAvatar className="self-center " />
+          <Avatar
+            as="button"
+            name={userData.username}
+            size="lg"
+            color="warning"
+            className="hover:scale-110 transition-transform"
+          />
+          <div style={{ borderRadius: '5px', overflow: 'hidden' }}></div>
         </div>
-        <p>Name: {userData?.username ?? 'None'}</p>
-        <p>Email: {userData?.email ?? 'None'}</p>
-        <p>Date Joined: {userData?.date ?? 'None'}</p>
+        <Text
+          h3
+          className="font-bold text-blue-800 dark:text-yellow-200 cursor-pointer inline break-all">
+          {userData?.username ?? 'None'}
+        </Text>
+
+        <Text className="font-bold text-blue-800 dark:text-yellow-200 cursor-pointer inline break-all">
+          User since {userData?.date ?? 'MM DD YYYY'}
+        </Text>
+        <p>{userData?.email ?? 'None'}</p>
       </div>
       <SignOutButton />
     </section>
