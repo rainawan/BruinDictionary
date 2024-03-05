e45cd1380f8eaf58245e31b1fc50b030c06218e2
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-03-04 16:37:04 -0800

    set default count

--- src/utils/getInfiniteEntriesQuery.js
+++ src/utils/getInfiniteEntriesQuery.js
@@ -3,7 +3,7 @@ import { useFirestoreInfiniteQuery } from '@react-query-firebase/firestore';
 import { db } from './firebase';
 
 // CURRENTLY ORDERING WITH termid OR userid ONLY WORKS FOR 'likes' AND 'creationDate'
-const getInfiniteEntriesQuery = (count, { termid, userid, order }) => {
+const getInfiniteEntriesQuery = (count = 10, { termid, userid, order }) => {
   const ref = query(
     collection(db, 'Entries'),
     ...(termid ? [where('termid', '==', termid)] : []),
