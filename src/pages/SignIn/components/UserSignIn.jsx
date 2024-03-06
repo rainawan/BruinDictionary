a8c255e6c11e2dfe909827c5a5f03a48c7516cd8
rainawan <rainawan02@gmail.com>
2024-03-06 12:26:28 -0800

    adjust div. minor

--- src/pages/SignIn/components/UserSignIn.jsx
+++ src/pages/SignIn/components/UserSignIn.jsx
@@ -39,49 +39,44 @@ const UserSignIn = () => {
   };
 
   return (
-    <div className="sign-in-container">
-      <form id="myForm" onSubmit={signIn}>
-        <div className="flex w-[400px] mx-auto flex-col flex-wrap mb-6 md:mb-0 gap-4">
-          <Input
-            label="Email"
-            size="md"
-            variant="bordered"
-            isRequired={true}
-            ref={email}
-            onClick={handleInputClick}
-            isInvalid={errorMessage.length > 0}
-            color={errorMessage.length ? 'danger' : 'default'} // text color of "email"
-          />
-          <Input
-            label="Password"
-            size="md"
-            variant="bordered"
-            isRequired={true}
-            ref={password}
-            isInvalid={errorMessage.length > 0}
-            color={errorMessage.length ? 'danger' : 'default'}
-            onClick={handleInputClick}
-            endContent={
-              <div className="m-auto mr-1 cursor-pointer" onClick={toggleVisibility}>
-                {isVisible ? (
-                  <EyeInvisibleFilled className="text-2xl text-default-400 pointer-events-none" />
-                ) : (
-                  <EyeFilled className="text-2xl text-default-400 pointer-events-none" />
-                )}
-              </div>
-            }
-            type={isVisible ? 'text' : 'password'}
-          />
-          {/* <p className="text-left text-[#f31260]">
-            {errorMessage && <p>Please enter valid credentials.</p>}
-          </p> */}
-          <p className="text-left text-[#f31260]">{errorMessage}</p>
-          <Button type="submit" color="primary">
-            Continue With Email
-          </Button>
-        </div>
-      </form>
-    </div>
+    <form id="myForm" onSubmit={signIn}>
+      <div className="flex w-[400px] mx-auto flex-col flex-wrap mb-6 md:mb-0 gap-4">
+        <Input
+          label="Email"
+          size="md"
+          variant="bordered"
+          isRequired={true}
+          ref={email}
+          onClick={handleInputClick}
+          isInvalid={errorMessage.length > 0}
+          color={errorMessage.length ? 'danger' : 'default'} // text color of "email"
+        />
+        <Input
+          label="Password"
+          size="md"
+          variant="bordered"
+          isRequired={true}
+          ref={password}
+          isInvalid={errorMessage.length > 0}
+          color={errorMessage.length ? 'danger' : 'default'}
+          onClick={handleInputClick}
+          endContent={
+            <div className="m-auto mr-1 cursor-pointer" onClick={toggleVisibility}>
+              {isVisible ? (
+                <EyeInvisibleFilled className="text-2xl text-default-400 pointer-events-none" />
+              ) : (
+                <EyeFilled className="text-2xl text-default-400 pointer-events-none" />
+              )}
+            </div>
+          }
+          type={isVisible ? 'text' : 'password'}
+        />
+        <p className="text-left text-[#f31260]">{errorMessage}</p>
+        <Button type="submit" color="primary">
+          Continue With Email
+        </Button>
+      </div>
+    </form>
   );
 };
 
