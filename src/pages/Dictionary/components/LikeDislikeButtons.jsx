6c90fc2941b8c44e52f1ad0eba765a7ba8a3ad08
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-03-05 21:12:17 -0800

    remove unused var

--- src/pages/Dictionary/components/LikeDislikeButtons.jsx
+++ src/pages/Dictionary/components/LikeDislikeButtons.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import { Button } from '@nextui-org/react';
 import { LikeFilled, DislikeFilled } from '@ant-design/icons';
 
