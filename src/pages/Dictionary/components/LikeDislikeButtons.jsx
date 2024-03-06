f9dc252740b505a59e0741180f2c527e43d2419e
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-03-05 21:11:31 -0800

    use one state instead of two

--- src/pages/Dictionary/components/LikeDislikeButtons.jsx
+++ src/pages/Dictionary/components/LikeDislikeButtons.jsx
@@ -3,43 +3,29 @@ import { Button } from '@nextui-org/react';
 import { LikeFilled, DislikeFilled } from '@ant-design/icons';
 
 const LikeDislikeButtons = ({ entry }) => {
-  const [like, setLike] = useState(false);
-  const [dislike, setDislike] = useState(false);
-  const prevAction = useRef(null);
+  // action can be either 'like' or 'dislike'
+  const [action, setAction] = useState(null);
 
-  const handleAction = (action) => {
-    const oppositeAction = action === 'like' ? 'dislike' : 'like';
-    if (prevAction.current === oppositeAction) {
-      if (oppositeAction === 'like') {
-        setLike(false);
-      } else {
-        setDislike(false);
-      }
-    }
-
-    if (action === 'like') {
-      setLike(prevAction.current !== 'like');
-    } else {
-      setDislike(prevAction.current !== 'dislike');
-    }
-
-    prevAction.current = prevAction.current !== action ? action : null;
+  const handleAction = (newAction) => {
+    setAction((prevAction) => (prevAction === newAction ? null : newAction));
   };
 
   return (
     <div className="inline-flex flex-row gap-1">
       <Button
-        className={`hover:text-green-500 ${like ? 'bg-green-500 text-white' : ''}`}
+        className={`hover:text-green-500 ${action === 'like' ? 'bg-green-500 text-white' : ''}`}
         onClick={() => handleAction('like')}>
         <LikeFilled className="text-lg" />
-        <p className="text-black dark:text-white">{like ? entry.likes + 1 : entry.likes}</p>
+        <p className="text-black dark:text-white">
+          {action === 'like' ? entry.likes + 1 : entry.likes}
+        </p>
       </Button>
       <Button
-        className={`hover:text-red-500 ${dislike ? 'bg-red-500 text-white' : ''}`}
+        className={`hover:text-red-500 ${action === 'dislike' ? 'bg-red-500 text-white' : ''}`}
         onClick={() => handleAction('dislike')}>
         <DislikeFilled className="text-lg" />
         <p className="text-black dark:text-white">
-          {dislike ? entry.dislikes + 1 : entry.dislikes}
+          {action === 'dislike' ? entry.dislikes + 1 : entry.dislikes}
         </p>
       </Button>
     </div>
