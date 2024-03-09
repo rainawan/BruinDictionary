082feb77f246ea0da2b01afb4dc194b8059db7e2
rainawan <rainawan02@gmail.com>
2024-03-08 17:39:15 -0800

    remove useRef, add formData

--- src/pages/SignIn/components/UserSignIn.jsx
+++ src/pages/SignIn/components/UserSignIn.jsx
@@ -1,12 +1,10 @@
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { auth } from '../../../utils/firebase';
 import { Input, Button } from '@nextui-org/react';
 import { EyeFilled, EyeInvisibleFilled } from '@ant-design/icons';
 
 const UserSignIn = () => {
-  const email = useRef();
-  const password = useRef();
   const [errorMessage, setErrorMessage] = useState('');
   const [isVisible, setIsVisible] = React.useState(false);
 
@@ -14,7 +12,10 @@ const UserSignIn = () => {
 
   const signIn = (e) => {
     e.preventDefault();
-    signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         console.log(userCredential);
       })
@@ -39,44 +40,50 @@ const UserSignIn = () => {
   };
 
   return (
-    <form id="myForm" onSubmit={signIn}>
-      <div className="flex w-[400px] mx-auto flex-col flex-wrap mb-6 md:mb-0 gap-4">
-        <Input
-          label="Email"
-          size="md"
-          variant="bordered"
-          isRequired={true}
-          ref={email}
-          onClick={handleInputClick}
-          isInvalid={errorMessage.length > 0}
-          color={errorMessage.length ? 'danger' : 'default'} // text color of "email"
-        />
-        <Input
-          label="Password"
-          size="md"
-          variant="bordered"
-          isRequired={true}
-          ref={password}
-          isInvalid={errorMessage.length > 0}
-          color={errorMessage.length ? 'danger' : 'default'}
-          onClick={handleInputClick}
-          endContent={
-            <div className="m-auto mr-1 cursor-pointer" onClick={toggleVisibility}>
-              {isVisible ? (
-                <EyeInvisibleFilled className="text-2xl text-default-400 pointer-events-none" />
-              ) : (
-                <EyeFilled className="text-2xl text-default-400 pointer-events-none" />
-              )}
-            </div>
-          }
-          type={isVisible ? 'text' : 'password'}
-        />
-        <p className="text-left text-[#f31260]">{errorMessage}</p>
-        <Button type="submit" color="primary">
-          Continue With Email
-        </Button>
-      </div>
-    </form>
+    <div>
+      <form id="myForm" onSubmit={signIn}>
+        <div className="flex w-[400px] mx-auto flex-col flex-wrap mb-6 md:mb-0 gap-4">
+          <Input
+            label="Email"
+            name="email"
+            type="email"
+            size="md"
+            variant="bordered"
+            isRequired={true}
+            // ref={email}
+            onClick={handleInputClick}
+            isInvalid={errorMessage.length > 0}
+            color={errorMessage.length ? 'danger' : 'default'} // text color of "email"
+          />
+          <Input
+            label="Password"
+            name="password"
+            type="password"
+            size="md"
+            variant="bordered"
+            isRequired={true}
+            // ref={password}
+            isInvalid={errorMessage.length > 0}
+            color={errorMessage.length ? 'danger' : 'default'}
+            onClick={handleInputClick}
+            endContent={
+              <div className="m-auto mr-1 cursor-pointer" onClick={toggleVisibility}>
+                {isVisible ? (
+                  <EyeInvisibleFilled className="text-2xl text-default-400 pointer-events-none" />
+                ) : (
+                  <EyeFilled className="text-2xl text-default-400 pointer-events-none" />
+                )}
+              </div>
+            }
+            // type={isVisible ? 'text' : 'password'}
+          />
+          <p className="text-left text-[#f31260]">{errorMessage}</p>
+          <Button type="submit" color="primary">
+            Continue With Email
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
 
