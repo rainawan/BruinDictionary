4799d05d90727327773a2f09fdff6552678d742f
Sonya Kim <96547645+sonyakim-dev@users.noreply.github.com>
2024-03-09 01:42:37 -0800

    use local storage

--- src/pages/Dictionary/components/LikeDislikeButtons.jsx
+++ src/pages/Dictionary/components/LikeDislikeButtons.jsx
@@ -6,10 +6,11 @@ const LIKE = 'true';
 const DISLIKE = 'false';
 
 const LikeDislikeButtons = ({ entry }) => {
-  // null means no previous action
+  // null action means no previous action
   const [action, setAction] = useState(localStorage.getItem(entry.id));
 
   const handleAction = (newAction) => {
+    let newActionState = null;
     // TODO: update firebase with new action
     switch (newAction) {
       case LIKE:
@@ -19,10 +20,12 @@ const LikeDislikeButtons = ({ entry }) => {
         } else if (action === DISLIKE) {
           // switched dislike to like -> inc like, dec dislike
           localStorage.setItem(entry.id, LIKE);
+          newActionState = LIKE;
         } else {
-          // when currA=== null
+          // when action === null
           // no previous action -> inc like
           localStorage.setItem(entry.id, LIKE);
+          newActionState = LIKE;
         }
         break;
       case DISLIKE:
@@ -32,35 +35,37 @@ const LikeDislikeButtons = ({ entry }) => {
         } else if (action === LIKE) {
           // switched like to dislike -> inc dislike, dec like
           localStorage.setItem(entry.id, DISLIKE);
+          newActionState = DISLIKE;
         } else {
           // when action === null
           // no previous action -> inc dislike
           localStorage.setItem(entry.id, DISLIKE);
+          newActionState = DISLIKE;
         }
         break;
       default:
         break;
     }
 
-    setAction((prevAction) => (prevAction === newAction ? null : newAction));
+    setAction(newActionState);
   };
 
   return (
     <div className="inline-flex flex-row gap-1">
       <Button
-        className={`hover:text-green-500 ${action === LIKE ? 'bg-green-500 text-white' : ''}`}
+        className={`hover:text-green-500 ${localStorage.getItem(entry.id) === LIKE ? 'bg-green-500 text-white' : ''}`}
         onClick={() => handleAction(LIKE)}>
         <LikeFilled className="text-lg" />
         <p className="text-black dark:text-white">
-          {action === LIKE ? entry.likes + 1 : entry.likes}
+          {localStorage.getItem(entry.id) === LIKE ? entry.likes + 1 : entry.likes}
         </p>
       </Button>
       <Button
-        className={`hover:text-red-500 ${action === DISLIKE ? 'bg-red-500 text-white' : ''}`}
+        className={`hover:text-red-500 ${localStorage.getItem(entry.id) === DISLIKE ? 'bg-red-500 text-white' : ''}`}
         onClick={() => handleAction(DISLIKE)}>
         <DislikeFilled className="text-lg" />
         <p className="text-black dark:text-white">
-          {action === DISLIKE ? entry.dislikes + 1 : entry.dislikes}
+          {localStorage.getItem(entry.id) === DISLIKE ? entry.dislikes + 1 : entry.dislikes}
         </p>
       </Button>
     </div>
