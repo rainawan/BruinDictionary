385caa0cc8fdf48fcdccc769dab8d0a374b99ebb
William Wong <wwong10th@gmail.com>
2024-03-11 15:46:47 -0700

    reverted to basic like/dislike functionality (no users)

--- src/pages/Dictionary/components/LikeDislikeButtons.jsx
+++ src/pages/Dictionary/components/LikeDislikeButtons.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react';
 import { Button } from '@nextui-org/react';
 import { LikeFilled, DislikeFilled } from '@ant-design/icons';
-import { doc } from 'firebase/firestore';
+import { doc, increment } from 'firebase/firestore';
 import { useFirestoreDocumentMutation } from '@react-query-firebase/firestore';
 import { db } from '../../../utils/firebase.js';
 
-const LIKE = 'true';
-const DISLIKE = 'false';
+const LIKE = true;
+const DISLIKE = false;
 
 const LikeDislikeButtons = ({ entry }) => {
   // null action means no previous action
-  const [action, setAction] = useState(localStorage.getItem(entry.id));
+  const [action, setAction] = useState(null);
 
   const entryDocRef = doc(db, 'Entries', entry.id);
-
   const mutation = useFirestoreDocumentMutation(entryDocRef, { merge: true });
 
   const handleAction = (newAction) => {
@@ -22,51 +21,48 @@ const LikeDislikeButtons = ({ entry }) => {
       case LIKE:
         if (action === LIKE) {
           // unclicked like -> dec like
-          localStorage.removeItem(entry.id);
           mutation.mutate({
-            likes: entry.likes - 1
+            likes: increment(-1)
           });
           setAction(null);
         } else {
           if (action === DISLIKE) {
             // switched dislike to like -> inc like, dec dislike
             mutation.mutate({
-              likes: entry.likes + 1,
-              dislikes: entry.dislikes - 1
+              likes: increment(1),
+              dislikes: increment(-1)
             });
           } else {
+            // when action === null
+            // no previous action -> inc like
             mutation.mutate({
-              likes: entry.likes + 1
+              likes: increment(1)
             });
           }
-          localStorage.setItem(entry.id, LIKE);
           setAction(LIKE);
         }
         break;
       case DISLIKE:
         if (action === DISLIKE) {
           // unclicked dislike -> dec dislike
-          localStorage.removeItem(entry.id);
           mutation.mutate({
-            dislikes: entry.dislikes - 1
+            dislikes: increment(-1)
           });
           setAction(null);
         } else {
           if (action === LIKE) {
             // switched like to dislike -> inc dislike, dec like
             mutation.mutate({
-              likes: entry.likes - 1,
-              dislikes: entry.dislikes + 1
+              likes: increment(-1),
+              dislikes: increment(1)
             });
-            setAction(DISLIKE);
           } else {
             // when action === null
             // no previous action -> inc dislike
             mutation.mutate({
-              dislikes: entry.dislikes + 1
+              dislikes: increment(1)
             });
           }
-          localStorage.setItem(entry.id, DISLIKE);
           setAction(DISLIKE);
         }
         break;
@@ -78,19 +74,19 @@ const LikeDislikeButtons = ({ entry }) => {
   return (
     <div className="inline-flex flex-row gap-1">
       <Button
-        className={`hover:text-green-500 ${localStorage.getItem(entry.id) === LIKE ? 'bg-green-500 text-white' : ''}`}
+        className={`hover:text-green-500 ${action === LIKE ? 'bg-green-500 text-white' : ''}`}
         onClick={() => handleAction(LIKE)}>
         <LikeFilled className="text-lg" />
         <p className="text-black dark:text-white">
-          {localStorage.getItem(entry.id) === LIKE ? entry.likes + 1 : entry.likes}
+          {action === LIKE ? entry.likes + 1 : entry.likes}
         </p>
       </Button>
       <Button
-        className={`hover:text-red-500 ${localStorage.getItem(entry.id) === DISLIKE ? 'bg-red-500 text-white' : ''}`}
+        className={`hover:text-red-500 ${action === DISLIKE ? 'bg-red-500 text-white' : ''}`}
         onClick={() => handleAction(DISLIKE)}>
         <DislikeFilled className="text-lg" />
         <p className="text-black dark:text-white">
-          {localStorage.getItem(entry.id) === DISLIKE ? entry.dislikes + 1 : entry.dislikes}
+          {action === DISLIKE ? entry.dislikes + 1 : entry.dislikes}
         </p>
       </Button>
     </div>
